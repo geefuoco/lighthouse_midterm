e9630e2a7e11d14269bb3041c3e1faeef549e262
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-06-03 20:39:20 -0400

    Added docstring and try/except blocks to get_data_sample function

--- src/modules/data_gathering.py
+++ src/modules/data_gathering.py
@@ -5,14 +5,28 @@ from dotenv import load_dotenv
 
 path = os.path.abspath(os.path.join(__file__, "../../../.env"))
 
-def get_data_sample(sample_size=100_000, table_name=None, q=None, forceRetrieve=False):
+def get_data_sample(table_name, sample_size=100_000, q=None, forceRetrieve=False):
+  """
+  Retrieves data from specified table and saves it into a data folder.
+  Returns a pandas Dataframe of the data.
+
+  *Note* this function expects a .env file in the root project directory with 
+  keys DB, HOSTNAME, USERNAME, PASSWORD in order to connect to the Postgres Database
+
+  sample_size = int: Number of samples to get (LIMIT value)\n
+  table_name = str: name of table in database\n
+  q = str: Query to the database. Appends a LIMIT to the end of the query\n
+  forceRetrieve = bool: Force query to database instead of reading from local file
+
+
+  """
   dir_path = os.path.abspath(os.path.join(__file__, "../../../data"))
-  data_path = os.path.abspath(os.path.join(__file__, "../../../data/sample.csv"))
+  data_path = os.path.abspath(os.path.join(__file__, f"../../../data/{table_name}_sample.csv"))
   data_file = os.path.exists(data_path)
   df = None
 
   if data_file and not forceRetrieve:
-    print("Data file exists. Retrieving from /data")
+    print(f"{table_name}_sample.csv file exists. Retrieving from /data")
     try:
       df = pd.read_csv(data_path)
     except:
@@ -33,10 +47,15 @@ def get_data_sample(sample_size=100_000, table_name=None, q=None, forceRetrieve=
     """
     connection = connect_to_db(dbparams)
     if connection:
-      if q and "limit" in q.lower():
-        df = pd.read_sql_query(q, connection)
-      else:
-        df = pd.read_sql_query(query, connection)
+      try:
+        if q:
+          df = pd.read_sql_query(q+f" LIMIT {sample_size}", connection)
+        else:
+          df = pd.read_sql_query(query, connection)
+      except:
+        print("An error has occured while querying to the database")
+        if q:
+          print(f"Provided query was : {q}")
       if not os.path.exists(dir_path):
         os.mkdir(dir_path)
       df.to_csv(data_path, index=False)
